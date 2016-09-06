bc0a6f6a630b6e35e19c1b17aeb631a8944c7542
iamevn <iamevn@users.noreply.github.com>
2016-09-05 23:42:04 -0400

    fixed, scaled down a ton

--- main.lua
+++ main.lua
@@ -8,7 +8,6 @@
 local res = {}
 s = "Hello world!"
 spos = { x = 250, y = 300 }
-center = { x = 300, y = 400 }
 mouseinfo = {
     pressed = false,
     dragged = { x = 0, y = 0 }
@@ -28,36 +27,21 @@ end
 -- run after love.update() each frame
 function love.draw()
     love.graphics.setColor(255, 0, 0, 255)
-    love.graphics.circle("fill", center.x, center.y, 1, 5)
+
     local x, y = spos.x, spos.y
-    -- love.graphics.printf( text, x, y, limit, align )
-
-    local mx, my = love.mouse.getPosition()
-    local foo, bar = 0, 0
-    if mx > center.x then
-        foo = 1
-    elseif mx < center.x then
-        foo = -1
-    end
-    if my > center.y then
-        bar = 1
-    elseif my < center.y then
-        bar = -1
-    end
-
-    for step = 10, 1, -1 do
+    for step = 0.5, 0.01, -0.01 do
         love.graphics.setColor(255 - step * 20, 255 - step * 20, 255 - step * 20, 255)
         love.graphics.printf(
             s,
             x + step * mouseinfo.dragged.x,
             y + step * mouseinfo.dragged.y,
-            600 - x,
+            800 - x,
             "left"
         )
     end
 
     love.graphics.setColor(255, 255, 255, 255)
-    love.graphics.printf(s, x, y, 600 - x, "left")
+    love.graphics.printf(s, x, y, 800 - x, "left")
 end
 
 -- https://love2d.org/wiki/KeyConstant
@@ -82,7 +66,6 @@ end
 
 function love.mousepressed(x, y, button)
     mouseinfo.pressed = true
-    mouseinfo.dragged = { x = 0, y = 0 }
 end
 
 function love.mousereleased(x, y, button)
