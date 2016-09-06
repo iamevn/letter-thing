b40875fd1273cf57b539007079979483ef08f9d6
iamevn <iamevn@users.noreply.github.com>
2016-09-05 23:49:43 -0400

    looks a little better

--- conf.lua
+++ conf.lua
@@ -1,6 +1,7 @@
 -- configuration options, run before anything else is loaded
 function love.conf(t)
     t.window.title = "letter-thing"
-    t.window.height = 800
-    t.window.width = 600
+    t.window.height = 600
+    t.window.width = 800
+    t.window.msaa = 4
 end
