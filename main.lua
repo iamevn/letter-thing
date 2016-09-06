aca10dead277f1171cec517c35ef856c2980c03d
iamevn <iamevn@users.noreply.github.com>
2016-09-05 21:32:03 -0400

    slight changes

--- main.lua
+++ main.lua
@@ -2,10 +2,14 @@
 -- To have dispatch start up love for you:
 -- :set makeprg=love\ .
 -- :autocmd BufWritePost *.lua Make!
+
 res = {}
+
+-- run once at init time
 function love.load()
     res.font = love.graphics.newFont("mentone-semibold.otf", 60)
     love.graphics.setFont(res.font)
+    love.graphics.setColor(255, 255, 255, 255)
 end
 
 function love.draw()
